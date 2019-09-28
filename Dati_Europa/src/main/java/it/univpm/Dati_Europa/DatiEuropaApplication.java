b64975f4c5a7022470b1b273e1398e9a74afb301
AndreaLapolla <48879270+AndreaLapolla@users.noreply.github.com>
2019-09-28 10:22:55 +0200

    Caricamento progetto
    Caricamento del progetto Dati_Europa sulla repository

---
+++ Dati_Europa/src/main/java/it/univpm/Dati_Europa/DatiEuropaApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.Dati_Europa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatiEuropaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatiEuropaApplication.class, args);
+	}
+
+}
