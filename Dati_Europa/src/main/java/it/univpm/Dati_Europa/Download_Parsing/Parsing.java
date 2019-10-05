1111426aa1fa36e2320a27b54ade7dfdf52a80c7
AndreaLapolla <48879270+AndreaLapolla@users.noreply.github.com>
2019-10-05 12:07:43 +0200

    Classe Parsing
    Prima implementazione della classe Parsing

--- Dati_Europa/src/main/java/it/univpm/Dati_Europa/Download_Parsing/Parsing.java
+++ Dati_Europa/src/main/java/it/univpm/Dati_Europa/Download_Parsing/Parsing.java
@@ -1,5 +1,106 @@
 package it.univpm.Dati_Europa.Download_Parsing;
 
-public class Parsing {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+//import java.io.FileReader;
+import java.io.IOException;
+//import it.univpm.Dati_Europa.Download_Parsing.*;
+//import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+//import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+//import org.json.simple.JSONArray;
+//import org.json.simple.JSONObject;
+//import org.json.simple.JSONValue;
+//import org.json.simple.parser.ParseException;
 
+public class Parsing {
+	ArrayList <String> Indici = new ArrayList <String>(); //lista che conterrà gli indici che descrivono i paesi nel csv
+	ArrayList <Double> Valori = new ArrayList <Double>(); //lista che conterrà i valori relativi agli indici per tutti i paesi del csv
+	private static final String DELIMETER_1 = "," ; // carattere separatore
+	private static final String DELIMETER_2 = "%" ; // carattere che permetterà di distinguere i double dalle stringhe
+	public Parsing(String link)
+	{
+		String riga = "" ;
+		BufferedReader br= null;
+		boolean bool1= false, bool2= false;
+		int limite=546;
+		int conta = 1;
+		try
+		{
+			URL urlCSV = new URL(link);
+			br = new BufferedReader(new InputStreamReader(urlCSV.openStream()));
+			while (((riga = br.readLine()) != null) && !bool2)
+			{
+				if (conta == limite) 
+				{
+					bool2= true;  // esce dal ciclo quando la variabile count è uguale a limit
+				}
+				if (!bool1)
+				{
+					bool1 = true;
+					continue; // permette di saltare la prima riga del csv
+				}
+				String[] Campi = riga.split(DELIMETER_1);  // dopo aver letto una riga del csv la divide ogni volta che trova il carattere separatore ","
+				for(String campo : Campi)
+				{
+					String new_campo = campo.replace(DELIMETER_2,""); // a tutti gli elementi del vettore di stringhe verrà rimosso il carattere percentuale, se presente
+					if(campo == new_campo) // se campo e new_campo sono uguali è perchè non è stato rimosso il carattere percentuale, in quanto non presente e quindi campo è uno degli indici
+					{
+						Indici.add(new_campo); // aggiunta alla lista degli indici
+					}
+					if(campo!=new_campo) // se sono diversi è perchè il carattere percentuale è stato trovato e rimosso, di conseguenza new_campo è un numero
+					{
+						Valori.add(Double.parseDouble(new_campo)); // conversione di new_campo in double e successiva aggiunta alla lista dei valori
+					}
+				}
+				conta++;
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (br != null)
+			{
+				try
+				{
+					br.close();
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println("Parsing completato.\n");
+		// stampa del contenuto delle due liste
+		/*System.out.println("Stampa lista degli indici:\n");
+		for(String indice : Indici)
+		{
+			System.out.println(indice + "\n");
+		}
+		System.out.println("Stampa lista dei valori:\n");
+		for(double valore : Valori)
+		{
+			System.out.println(valore + "\n");
+		}*/
+	}
+	// metodi per la restituzione delle liste
+	public List<String> getIndici()
+	{
+		return Indici;
+	}
+	public List<Double> getValori()
+	{
+		return Valori;
+	}
 }
