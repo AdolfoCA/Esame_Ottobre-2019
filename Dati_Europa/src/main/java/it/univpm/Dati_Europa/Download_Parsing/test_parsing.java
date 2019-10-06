f4233a193d8bfaf7612e984ba7a9097a614f4833
AndreaLapolla <48879270+AndreaLapolla@users.noreply.github.com>
2019-10-06 11:48:44 +0200

    Modifiche
    Modifiche classe Parsing

--- Dati_Europa/src/main/java/it/univpm/Dati_Europa/Download_Parsing/test_parsing.java
+++ Dati_Europa/src/main/java/it/univpm/Dati_Europa/Download_Parsing/test_parsing.java
@@ -7,38 +7,6 @@ public class test_parsing {
 	public static void main ( String args [])
 	{
 		Parsing p1;
-		Download d1;
-		d1 = new Download("http://data.europa.eu/euodp/data/api/3/action/package_show?id=fifth-european-working-conditions-survey-2010");
-		p1 = new Parsing(d1.Getlink());
-		// stampa del contenuto delle due liste
-		System.out.println("Stampa lista degli indici:\n");
-		for(String indice : p1.getIndici())
-		{
-			System.out.println(indice + "\n");
-		}
-		System.out.println("Stampa lista dei valori:\n");
-		for(double valore : p1.getValori())
-		{
-			System.out.println(valore + "\n");
-		}
-		//prove per la classe parsing
-		/*String var1 = "";
-		Integer var2 = 1;
-		Double var3 = 1.1;
-		Boolean var4 = false;
-		System.out.println(var1 + " " + var1.getClass().getSimpleName());
-		System.out.println(var2 + " " + var2.getClass().getSimpleName());
-		System.out.println(var3 + " " + var3.getClass().getSimpleName());
-		System.out.println(var4 + " " + var4.getClass().getSimpleName());
-		String errore = "333%";
-		String corretto = errore.replace("%", "");
-		System.out.println(corretto + " " + corretto.getClass().getSimpleName());
-		if(errore!=corretto)
-		{
-			double Dcorretto = Double.parseDouble(corretto);
-			corretto=corretto+3;
-			Dcorretto=Dcorretto+3;
-			System.out.println(corretto + " " + Dcorretto);
-		}*/
+		p1 = new Parsing("http://www.eurofound.europa.eu/surveys/ewcs/2010/documents/table5.csv");
 	}
 }
